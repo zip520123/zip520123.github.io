816fe28ffc06c8e8f2ccaf66d98708378f5e26be
zip520123 <zip520123@yahoo.com.tw>
2021-01-26 17:07:45 +0000

    update description

--- src/BigCard.js
+++ src/BigCard.js
@@ -20,14 +20,15 @@ class BigCard extends Component {
                         <h3 class="mdl-cell mdl-cell--12-col mdl-typography--headline">Introduction</h3>
                         <div class="mdl-cell mdl-cell--8-col mdl-card__supporting-text no-padding ">
                             <p>
-                                Excepteur reprehenderit sint exercitation ipsum consequat qui sit id velit elit. Velit anim eiusmod labore sit amet. Voluptate voluptate irure occaecat deserunt incididunt esse in. Sunt velit aliquip sunt elit ex nulla reprehenderit qui ut eiusmod ipsum do. Duis veniam reprehenderit laborum occaecat id proident nulla veniam. Duis enim deserunt voluptate aute veniam sint pariatur exercitation. Irure mollit est sit labore est deserunt pariatur duis aute laboris cupidatat. Consectetur consequat esse est sit veniam adipisicing ipsum enim irure.
+                            We don’t have right to judge we don’t understand.
                             </p>
                         </div>
-                        <h3 class="mdl-cell mdl-cell--12-col mdl-typography--headline">Irure mollit est sit labore</h3>
+                        <h3 class="mdl-cell mdl-cell--12-col mdl-typography--headline">Is the shadow in the moonlight inevitable?</h3>
 
                         <div class="mdl-cell mdl-cell--8-col mdl-card__supporting-text no-padding ">
                             <p>
-                                Excepteur reprehenderit sint exercitation ipsum consequat qui sit id velit elit. Velit anim eiusmod labore sit amet. Voluptate voluptate irure occaecat deserunt incididunt esse in. Sunt velit aliquip sunt elit ex nulla reprehenderit qui ut eiusmod ipsum do. Duis veniam reprehenderit laborum occaecat id proident nulla veniam. Duis enim deserunt voluptate aute veniam sint pariatur exercitation. Irure mollit est sit labore est deserunt pariatur duis aute laboris cupidatat. Consectetur consequat esse est sit veniam adipisicing ipsum enim irure.
+                            If you stop forward simply because you don’t have right to judge you don’t understand.
+You eventually stay where you’re forever.
                             </p>
                         </div>
                     </div>
